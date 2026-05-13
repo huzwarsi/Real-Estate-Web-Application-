64cb40a654812a5798a2f463bdc9271978dee24c
huzwarsi <huzaifawarsi2006@gmail.com>
2026-05-13 23:01:57 +0500

    Login Signup Done

--- Server/Controller/authContoller.js
+++ Server/Controller/authContoller.js
@@ -7,34 +7,72 @@ const register = async(req,res)=>{
 
     const {email,userName , password} = req.body
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-// database Operations
-console.log(email,userName,hashedPassword);
+    try{
 
-
-// Create a new user in the database using Prisma
-
-const newUser = await prisma.User.create({
-    data:{
-        email, 
-        userName,
-        password:hashedPassword
+        
+        const hashedPassword = await bcrypt.hash(password, 10);
+        // database Operations
+        console.log(email,userName,hashedPassword);
+        
+        
+        // Create a new user in the database using Prisma
+        
+        const newUser = await prisma.User.create({
+            data:{
+                email, 
+                userName,
+                password:hashedPassword
+            }
+        })
+        
+        
+        
+        
+        res.json({
+            message:'User registered successfully',
+            user: newUser
+        })
+    }catch(error){
+        console.error('Error registering user:', error);
+        res.status(500).json({ message: 'Internal server error' });
     }
-})
-
-
-
-
-res.json({
-    message:'User registered successfully',
-    user: newUser
-})
 
 }
 
-const login = (req,res)=>{
+const login = async (req,res)=>{
+
+    const {email,password} = req.body
+
+    try{
+
+        
+        const user = await prisma.User.findUnique({
+            where:{
+                email
+            }})
+            if(!user){
+             return res.json({ message: 'Invalid Credintials' });
+
+            }
+
+
+        const ispasswordValid = await bcrypt.compare(password, user.password);
+            if(!ispasswordValid){
+             return res.json({ message: 'Invalid Credintials' });
+       
+
+            }
+            res.json({ message: 'Login Successfully' });
+            
+
+    }catch(error){
+        console.error('Error logging in user:', error);
+        res.status(500).json({ message: 'Error' });
+    }
 
 // database Operations
+
+
 
 }
 
